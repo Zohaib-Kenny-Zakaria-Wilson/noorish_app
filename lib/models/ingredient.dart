81785bbcb0f26cd693739659274f2e067fb0b158
ZakRab <107758804+ZakRab@users.noreply.github.com>
2024-02-17 14:00:26 -0600

    Co-authored-by: Zohaib2421 <Zohaib2421@users.noreply.github.com>

---
+++ lib/models/ingredient.dart
@@ -0,0 +1,30 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'ingredient.g.dart';
+
+@JsonSerializable()
+class Ingredient {
+  final String name;
+  final int quantity;
+  final int quantityConsumed;
+  final int protien;
+  final int carbs;
+  final int calories;
+  final int fats;
+  final double servingSize;
+
+  factory Ingredient.fromJson(
+          Map<String, dynamic>
+              json) => // converts json to actual instianiated objects
+      _$IngredientFromJson(json);
+
+  Ingredient(
+      {required this.name,
+      required this.quantity,
+      required this.protien,
+      required this.carbs,
+      required this.calories,
+      required this.fats,
+      required this.servingSize,
+      required this.quantityConsumed});
+}
---
+++ lib/models/ingredient.g.dart
@@ -0,0 +1,30 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'ingredient.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+Ingredient _$IngredientFromJson(Map<String, dynamic> json) => Ingredient(
+      name: json['name'] as String,
+      quantity: json['quantity'] as int,
+      protien: json['protien'] as int,
+      carbs: json['carbs'] as int,
+      calories: json['calories'] as int,
+      fats: json['fats'] as int,
+      servingSize: (json['servingSize'] as num).toDouble(),
+      quantityConsumed: json['quantityConsumed'] as int,
+    );
+
+Map<String, dynamic> _$IngredientToJson(Ingredient instance) =>
+    <String, dynamic>{
+      'name': instance.name,
+      'quantity': instance.quantity,
+      'quantityConsumed': instance.quantityConsumed,
+      'protien': instance.protien,
+      'carbs': instance.carbs,
+      'calories': instance.calories,
+      'fats': instance.fats,
+      'servingSize': instance.servingSize,
+    };
