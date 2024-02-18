c095634e8e7795a46002173219acc4be989c7d42
Kenny Morales <kenny-morales@outlook.com>
2024-02-18 01:44:57 -0600

    Finished Meal Container for day page

--- lib/pages/day_page.dart
+++ lib/pages/day_page.dart
@@ -28,17 +28,19 @@ class _DayPageState extends State<DayPage> {
 
   Widget _renderMealContainer(BuildContext context, meals) {
     return Padding(
+      padding: const EdgeInsets.all(24.0),
       child: Column(children: [
         Align(
           alignment: Alignment.centerLeft,
           child: Text('Meals', style: Styles.headline1),
         ),
+        const SizedBox(height: 16.0), // Gap after button
         _renderAddMealButton(context),
+        const SizedBox(height: 16.0), // Gap after button
         Expanded(
           child: _renderMealList(context, meals),
         ),
       ]),
-      padding: EdgeInsets.all(24.0),
     );
   }
 
@@ -49,9 +51,10 @@ class _DayPageState extends State<DayPage> {
           borderRadius:
               BorderRadius.circular(10), // Explicitly making corners rounded
         ),
-        backgroundColor: Colors.green[500],
+        backgroundColor: Styles.secondaryColor,
       ),
       child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Icon(
             Icons.add,
@@ -62,7 +65,6 @@ class _DayPageState extends State<DayPage> {
             style: Styles.baseTextStyle,
           )
         ],
-        mainAxisAlignment: MainAxisAlignment.center,
       ),
       onPressed: () => _navMeal(context, today),
     );
@@ -79,19 +81,26 @@ class _DayPageState extends State<DayPage> {
     final totalCalories = meal.ingredientsList
         .fold<int>(0, (sum, current) => sum + current.calories);
 
-    return Card(
-      shape: RoundedRectangleBorder(
-        borderRadius:
-            BorderRadius.circular(10), // Explicitly making corners rounded
-      ),
-      child: ListTile(
-        leading: Icon(Icons.fastfood), // Example icon
-        title:
-            Text(meal.name, style: Styles.baseTextStyle), // Display meal name
-        subtitle: Text('Calories: ${totalCalories}'), // Example detail
-        onTap: () {
-          // Handle tap if needed, for example to edit the meal
-        },
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 12.0),
+      child: Card(
+        shape: RoundedRectangleBorder(
+          borderRadius:
+              BorderRadius.circular(10), // Explicitly making corners rounded
+        ),
+        child: ListTile(
+          leading: const Icon(Icons.fastfood), // Example icon
+          title: Text(meal.name,
+              style: Styles.baseTextStyle
+                  .copyWith(color: Styles.secondaryColor)), // Display meal name
+          subtitle: Text(
+            'Calories: $totalCalories',
+            style: Styles.descriptiveTextStyle,
+          ), // Example detail
+          onTap: () {
+            // Handle tap if needed, for example to edit the meal
+          },
+        ),
       ),
     );
   }
--- lib/style.dart
+++ lib/style.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 
 class Styles {
   static final Color backgroundColor = Color.fromARGB(255, 243, 248, 255);
+  static final Color primaryColor = Color.fromARGB(255, 0, 167, 156);
+  static final Color secondaryColor = Color.fromARGB(255, 43, 182, 115);
   static final Color primaryTextColor = Color.fromARGB(255, 30, 30, 30);
 
   ///TextStyles
@@ -16,14 +18,19 @@ class Styles {
     fontFamily: _fontNameDefault,
     fontSize: 16.0,
     fontWeight: FontWeight.w400,
-    color: primaryTextColor,
+  );
+
+  static final TextStyle descriptiveTextStyle = TextStyle(
+    fontFamily: _fontNameDefault,
+    fontSize: 14.0,
+    fontWeight: FontWeight.w300,
   );
 
   // Specific text styles
   static final TextStyle headline1 = TextStyle(
+    fontFamily: _fontNameDefault,
     fontSize: 24.0,
     fontWeight: FontWeight.w500,
-    color: primaryTextColor,
   );
 
   ///TextStyles
