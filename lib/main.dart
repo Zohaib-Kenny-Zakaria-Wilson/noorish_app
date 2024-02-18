b9ec4f81929f31cc01ed8e835abbcc0a9fff94c6
ZakRab <107758804+ZakRab@users.noreply.github.com>
2024-02-17 19:32:01 -0600

    adds app bar nav

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,38 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:noorish_app/pages/calendar_page.dart';
+import 'package:noorish_app/pages/day_page.dart';
+import 'package:noorish_app/pages/reciepe_page.dart';
 import 'package:noorish_app/style.dart';
 import 'package:noorish_app/mocks/user_mock.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Scaffold(
-      appBar: AppBar(
-        title: Text("Hello World!", style: Style.fonts),
+    home: DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Color.fromARGB(255, 46, 103, 156),
+          title: const Text(
+            'NOORISH',
+            textAlign: TextAlign.center,
+          ),
+        ),
+        body: const TabBarView(
+          children: [
+            Calendar(),
+            DayPage(),
+            Recipe(),
+          ],
+        ),
+        bottomNavigationBar: const TabBar(
+          tabs: [
+            Tab(icon: Icon(Icons.calendar_today_rounded)),
+            Tab(icon: Icon(Icons.home_filled)),
+            Tab(icon: Icon(Icons.list_alt_rounded)),
+          ],
+          padding: EdgeInsets.all(16.0),
+        ),
       ),
     ),
   ));
--- lib/pages/day_page.dart
+++ lib/pages/day_page.dart
@@ -1,1 +1,15 @@
+import 'package:flutter/material.dart';
 
+class DayPage extends StatefulWidget {
+  const DayPage({super.key});
+
+  @override
+  State<DayPage> createState() => _DayPageState();
+}
+
+class _DayPageState extends State<DayPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
